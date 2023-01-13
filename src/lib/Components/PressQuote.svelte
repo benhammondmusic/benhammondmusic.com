4b7bc81f0fe4bcea77d2f11a33d6c9beddf4c280
Ben Hammond <benjamin.hammond@gmail.com>
2023-01-13 11:41:44 -0700

    tweak press quotes

--- src/lib/Components/PressQuote.svelte
+++ src/lib/Components/PressQuote.svelte
@@ -5,8 +5,8 @@
 	export let quoteOrg: string;
 </script>
 
-<div
-	class="mx-auto mt-24 w-full rounded-lg bg-bhm-sky-50 px-5 pt-5 pb-10 text-bhm-gray-800 shadow-lg"
+<article
+	class="mx-auto w-full rounded-lg bg-bhm-sky-50 py-5 px-5 pb-10 text-bhm-gray-800 shadow-lg"
 	style="max-width: 500px"
 >
 	<div class="w-full pt-1 pb-5">
@@ -19,7 +19,7 @@
 	</div>
 	<div class="mb-10 w-full">
 		<div class="h-3 text-left text-3xl leading-tight text-bhm-blue-500">“</div>
-		<p class="px-5 text-center text-sm text-bhm-gray-600">
+		<p class="px-5 text-center italic text-bhm-gray-600">
 			{quoteText}
 		</p>
 		<div class="-mt-3 h-3 text-right text-3xl leading-tight text-bhm-blue-500">”</div>
@@ -32,4 +32,4 @@
 			{quoteOrg}
 		</p>
 	</div>
-</div>
+</article>
--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -8,114 +8,119 @@
 	<title>Ben Hammond - Original Music</title>
 </svelte:head>
 
-<FancyHeading text={"Albums"} />
-
-<section
-	class="grid grid-cols-1 justify-items-center gap-10 align-top sm:grid-cols-2 2xl:grid-cols-4"
->
-	<AlbumItem
-		title="Gone (To California)"
-		subtitle="Video and Single - 2017"
-		src="/images/albumcovers/gone.jpg"
-		spotifyUrl="https://open.spotify.com/album/0wO71kn47uK9AA5J4NqbN9"
-		appleMusicUrl="https://music.apple.com/us/album/gone-to-california-single/1231607849"
-		amazonUrl="https://www.amazon.com/dp/B071YQK9BL/ref=ap_ws_tlw_trk2"
+<section>
+	<FancyHeading text={"Albums"} />
+	<div
+		class="grid grid-cols-1 justify-items-center gap-10 align-top sm:grid-cols-2 2xl:grid-cols-4"
 	>
-		<p>
-			Featuring <a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://www.johnpatti.com">John Patti</a
-			>. Video by
-			<a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://joey-durango.com/">Joey Durango</a
-			>. Song by Matt Gerhardt.
-		</p>
-	</AlbumItem>
+		<AlbumItem
+			title="Gone (To California)"
+			subtitle="Video and Single - 2017"
+			src="/images/albumcovers/gone.jpg"
+			spotifyUrl="https://open.spotify.com/album/0wO71kn47uK9AA5J4NqbN9"
+			appleMusicUrl="https://music.apple.com/us/album/gone-to-california-single/1231607849"
+			amazonUrl="https://www.amazon.com/dp/B071YQK9BL/ref=ap_ws_tlw_trk2"
+		>
+			<p>
+				Featuring <a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://www.johnpatti.com">John Patti</a
+				>. Video by
+				<a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://joey-durango.com/">Joey Durango</a
+				>. Song by Matt Gerhardt.
+			</p>
+		</AlbumItem>
 
-	<AlbumItem
-		title="Friction"
-		subtitle="Studio Album - 2014"
-		src="/images/albumcovers/friction.jpg"
-		spotifyUrl="https://open.spotify.com/album/0wO71kn47uK9AA5J4NqbN9"
-		appleMusicUrl="https://music.apple.com/us/album/friction/920576506"
-		amazonUrl="https://www.amazon.com/Friction-Ben-Hammond/dp/B00NP82T0I/"
-	>
-		<p>
-			Co-Produced with <a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://www.instagram.com/matthewfrostmusic/?hl=en">Matt Frost</a
-			>. Full Band Originals and Solo, Live-Looped Covers ft.
-			<a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://www.youtube.com/user/kindredsheeler/videos">Kindred</a
-			> and many more.
-		</p>
-	</AlbumItem>
+		<AlbumItem
+			title="Friction"
+			subtitle="Studio Album - 2014"
+			src="/images/albumcovers/friction.jpg"
+			spotifyUrl="https://open.spotify.com/album/0wO71kn47uK9AA5J4NqbN9"
+			appleMusicUrl="https://music.apple.com/us/album/friction/920576506"
+			amazonUrl="https://www.amazon.com/Friction-Ben-Hammond/dp/B00NP82T0I/"
+		>
+			<p>
+				Co-Produced with <a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://www.instagram.com/matthewfrostmusic/?hl=en">Matt Frost</a
+				>. Full Band Originals and Solo, Live-Looped Covers ft.
+				<a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://www.youtube.com/user/kindredsheeler/videos">Kindred</a
+				> and many more.
+			</p>
+		</AlbumItem>
 
-	<AlbumItem
-		title="Solo at Stone Mt Arts Center"
-		subtitle="Live Album and DVD - 2011"
-		src="/images/albumcovers/solo.jpg"
-		spotifyUrl="https://open.spotify.com/album/3DTF0fum9uhqGxOS2czdaf"
-		appleMusicUrl="https://music.apple.com/us/album/solo-at-stone-mt-arts-center/417555127"
-		amazonUrl="https://www.amazon.com/Solo-Stone-Mt-Arts-Center/dp/B004MFOCQW/"
-	>
-		<p>
-			Recorded live, minutes from Ben's hometown in rural Maine. Features solo originals and cover
-			songs. Video by Michael Dana.
-		</p>
-	</AlbumItem>
+		<AlbumItem
+			title="Solo at Stone Mt Arts Center"
+			subtitle="Live Album and DVD - 2011"
+			src="/images/albumcovers/solo.jpg"
+			spotifyUrl="https://open.spotify.com/album/3DTF0fum9uhqGxOS2czdaf"
+			appleMusicUrl="https://music.apple.com/us/album/solo-at-stone-mt-arts-center/417555127"
+			amazonUrl="https://www.amazon.com/Solo-Stone-Mt-Arts-Center/dp/B004MFOCQW/"
+		>
+			<p>
+				Recorded live, minutes from Ben's hometown in rural Maine. Features solo originals and cover
+				songs. Video by Michael Dana.
+			</p>
+		</AlbumItem>
 
-	<AlbumItem
-		title="[Reasonably] Honest"
-		subtitle="Studio Album - 2008"
-		src="/images/albumcovers/reasonablyhonest.jpg"
-		spotifyUrl="https://open.spotify.com/album/4P8v3P5so2Re7e2JcvuluA"
-		appleMusicUrl="https://music.apple.com/us/album/reasonably-honest-extended-edition/350749973"
-		amazonUrl="https://www.amazon.com/Reasonably-Honest-Ben-Hammond/dp/B0038AN416/"
-	>
-		<p>
-			Co-Produced with <a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://mojitomastering.com/">Reuben Ghose</a
-			>. Original songs ft.
-			<a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://www.zachfinkelstein.com/">Zach Finkelstein</a
-			>,
-			<a
-				class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
-				href="https://www.jonathanlindhorst.com/">Jon Lindhorst</a
-			>, and more.
-		</p>
-	</AlbumItem>
+		<AlbumItem
+			title="[Reasonably] Honest"
+			subtitle="Studio Album - 2008"
+			src="/images/albumcovers/reasonablyhonest.jpg"
+			spotifyUrl="https://open.spotify.com/album/4P8v3P5so2Re7e2JcvuluA"
+			appleMusicUrl="https://music.apple.com/us/album/reasonably-honest-extended-edition/350749973"
+			amazonUrl="https://www.amazon.com/Reasonably-Honest-Ben-Hammond/dp/B0038AN416/"
+		>
+			<p>
+				Co-Produced with <a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://mojitomastering.com/">Reuben Ghose</a
+				>. Original songs ft.
+				<a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://www.zachfinkelstein.com/">Zach Finkelstein</a
+				>,
+				<a
+					class="text-bhm-copper-300 hover:bg-bhm-copper-700 hover:text-white"
+					href="https://www.jonathanlindhorst.com/">Jon Lindhorst</a
+				>, and more.
+			</p>
+		</AlbumItem>
+	</div>
 </section>
 
-<section>
-	<PressQuote
-		quotePic="/images/reviewers/raneelee.jpg"
-		quoteText="His beautiful lyrics, ranging from sensual to exuberance and everything in between come together nicely in a great package of vocal performance and instrumental gifts."
-		quoteName="Ranee Lee"
-		quoteOrg="Award-winning Jazz Vocalist"
-	/>
-	<PressQuote
-		quotePic="/images/reviewers/portlandpressherald.png"
-		quoteText="Hammond is a versatile musician who writes memorable originals, delivers when he decides to cover someone, and uses his guitar, voice and gadgets to create a sound that you want to keep hearing."
-		quoteName="Aimsel Ponti"
-		quoteOrg="Portland Press Herald"
-	/>
-	<PressQuote
-		quotePic="/images/reviewers/moss.jpg"
-		quoteText="He was awesome! His looping and talent with an array of instruments allows him to play anything from background music to dance party... a talented musician and a great guy, highly recommended for a wedding or any other special event."
-		quoteName="Jordan Johnson"
-		quoteOrg="Owner of Moss in Denver"
-	/>
+<section class="mt-24">
+	<FancyHeading text={"Press Quotes"} />
 
-	<PressQuote
-		quotePic="/images/reviewers/ticket.jpg"
-		quoteText="...one minute he's singer-songwriter smooth, the next breaking into rap-jazzy scats, skillfully co-merging genres within songs."
-		quoteName="Dawn Scire"
-		quoteOrg="Sarasota Herald Tribune (Ticket)"
-	/>
+	<div class="grid grid-cols-4 gap-5 pt-10">
+		<PressQuote
+			quotePic="/images/reviewers/raneelee.jpg"
+			quoteText="His beautiful lyrics, ranging from sensual to exuberance and everything in between come together nicely in a great package of vocal performance and instrumental gifts."
+			quoteName="Ranee Lee"
+			quoteOrg="Award-winning Jazz Vocalist"
+		/>
+		<PressQuote
+			quotePic="/images/reviewers/portlandpressherald.png"
+			quoteText="Hammond is a versatile musician who writes memorable originals, delivers when he decides to cover someone, and uses his guitar, voice and gadgets to create a sound that you want to keep hearing."
+			quoteName="Aimsel Ponti"
+			quoteOrg="Portland Press Herald"
+		/>
+		<PressQuote
+			quotePic="/images/reviewers/moss.jpg"
+			quoteText="He was awesome! His looping and talent with an array of instruments allows him to play anything from background music to dance party... a talented musician and a great guy, highly recommended for a wedding or any other special event."
+			quoteName="Jordan Johnson"
+			quoteOrg="Owner of Moss in Denver"
+		/>
+
+		<PressQuote
+			quotePic="/images/reviewers/ticket.jpg"
+			quoteText="...one minute he's singer-songwriter smooth, the next breaking into rap-jazzy scats, skillfully co-merging genres within songs."
+			quoteName="Dawn Scire"
+			quoteOrg="Sarasota Herald Tribune (Ticket)"
+		/>
+	</div>
 </section>
