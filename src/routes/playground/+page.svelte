b2ba346bc06c72540cd25f077cc78fba8a86dd72
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-18 13:50:00 -0600

    spinner; center playrgound, cleanup

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { PUBLIC_BASE_URL } from "$env/static/public";
-
+	import { fade } from "svelte/transition";
+	import FancyHeading from "$lib/Components/FancyHeading.svelte";
+	import { Wave } from "svelte-loading-spinners";
 	import {
 		asPct,
 		getAverageOfProperty,
@@ -24,33 +26,65 @@
 	const featuresToAverage: Feature[] = ["danceability", "energy", "acousticness", "valence"];
 </script>
 
-{#await songDataPromise}
-	<p>...waiting</p>
-{:then songData}
-	<div>
-		<DonutChart data={getArtistCounts(songData.data)} />
-		<PieChart valueCounts={getKeySigsCounts(songData.data)} />
-	</div>
-	<p>
-		Average tempo:
-		<span class="font-bold">{Math.round(getAverageOfProperty(songData.data, "tempo"))} bpm</span>
-	</p>
-	{#each featuresToAverage as feature (feature)}
-		<p>
-			Average {feature}:
-			<span class="font-bold">{asPct(getAverageOfProperty(songData.data, feature))} </span>
+<svelte:head>
+	<title>Ben Hammond - Music Data Viz Playground</title>
+</svelte:head>
+
+<section
+	transition:fade
+	class="align-center mx-auto w-full max-w-7xl"
+>
+	<FancyHeading text={"Music Data-Viz Playground"} />
+
+	<div class="flex flex-col items-center">
+		<p class="max-w-xl pb-10">
+			Below are some ongoing experiments in visualizing the data behind my music career. I have
+			uploaded (most) of my songlist as a Spotify playlist; that data is securely fetched, wrangled,
+			and ultimately displayed below. Much of my data viz experience comes from my full-time job as
+			Senior Software Engineer on the <a
+				href="https://healthequitytracker.org"
+				class="underline hover:bg-bhm-sky">Health Equity Tracker</a
+			>. I will be using this page as a low-stakes way to try new ideas and explore SvelteKit and D3
+			creations.
 		</p>
-	{/each}
 
-	<p>
-		Most common key: {getMostCommonKeySigs(songData.data).join(", ")}
-	</p>
+		{#await songDataPromise}
+			<Wave
+				size="120"
+				color="#336699"
+				unit="px"
+				duration="2s"
+			/>
+			<article class="flex justify-center">
+				<p>loading songlist metadata from Spotify...</p>
+			</article>
+		{:then songData}
+			<article class="rounded-sm ring-1 ring-bhm-blue p-5">
+				<p>
+					Average tempo:
+					<span class="font-bold"
+						>{Math.round(getAverageOfProperty(songData.data, "tempo"))} bpm</span
+					>
+				</p>
+				{#each featuresToAverage as feature (feature)}
+					<p>
+						Average {feature}:
+						<span class="font-bold">{asPct(getAverageOfProperty(songData.data, feature))} </span>
+					</p>
+				{/each}
 
-	<div class="grid grid-cols-4 mt-24">
-		{#each songData.data as song (song.id)}
-			<span class="text-xs">{song.name.split(" - ")[0]}</span>
-		{/each}
+				<p>
+					Most common key: <span class="font-bold"
+						>{getMostCommonKeySigs(songData.data).join(", ")}</span
+					>
+				</p>
+			</article>
+			<div>
+				<DonutChart data={getArtistCounts(songData.data)} />
+				<!-- <PieChart valueCounts={getKeySigsCounts(songData.data)} /> -->
+			</div>
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
 	</div>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+</section>
