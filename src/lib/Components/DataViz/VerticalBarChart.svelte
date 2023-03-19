f102ef4f07996590524b20f72ca552ce53e4e89a
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-19 09:50:11 -0600

    tempo working on desktop

--- src/lib/Components/DataViz/VerticalBarChart.svelte
+++ src/lib/Components/DataViz/VerticalBarChart.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import type { Song } from "$lib/utils/songlistUtils";
 	export let songs: Song[];
-	import { scaleLinear } from "d3-scale";
-	import { slide } from "svelte/transition";
-
-	console.log(songs);
+	import { scaleLinear, scaleBand } from "d3-scale";
+	import { flip } from "svelte/animate";
 
 	const tempoMarkings = [
 		[50, "Lento"],
@@ -17,7 +15,18 @@
 		[220, "Prestissimo"],
 	];
 
-	const tempoDistributions = getTempoDistributions(songs);
+	const colors = [
+		"#336699",
+		"#5b64a0",
+		"#815f9e",
+		"#a15a94",
+		"#ba5681",
+		"#ca5769",
+		"#ce614f",
+		"#c87136",
+	];
+
+	const data = getTempoDistributions(songs);
 
 	function getTempoBucket(tempo: number) {
 		for (let [maxTempo, bucket] of tempoMarkings) {
@@ -25,13 +34,6 @@
 		}
 		return "Weird Tempo";
 	}
-
-	const xTicks = tempoMarkings.map((item) => item[1]);
-	const yTicks = [50, 100, 150, 200, 250];
-	const padding = { top: 20, right: 15, bottom: 20, left: 25 };
-
-	let width = 1200;
-	let height = 1500;
 
 	function getTempoDistributions(songs: Song[]) {
 		let tempoDistributions: Record<string, number> = { Lento: 0 };
@@ -47,66 +49,122 @@
 		return tempoDistributions;
 	}
 
-	$: xScale = scaleLinear()
-		.domain([0, xTicks.length])
-		.range([padding.left, width - padding.right]);
+	const marginTop = 15; // top margin, in pixels
+	const marginRight = 2; // right margin, in pixels
+	const marginBottom = 30; // bottom margin, in pixels
+	const marginLeft = 50; // left margin, in pixels
+	const width = 800; // width of the chart, in pixels
+	const height = 700; // height of the chart, in pixels
+	const xPadding = 0.2; // padding between bars
+	const yFormat = " songs"; // unit to display on y-axis ticks
+	const yLabel = "↑ Number of songs per tempo"; // label for the y-axis
+	const yScaleFactor = 2; // number of ticks on y-axis
 
-	console.log(xScale);
+	$: reactiveXVals = tempoMarkings.map((row) => row[1]);
+	$: reactiveYVals = tempoMarkings.map((row) => data[row[1]]);
 
-	$: yScale = scaleLinear()
-		.domain([0, Math.max.apply({ "0": 0 }, yTicks)])
-		.range([height - padding.bottom, padding.top]);
+	// Compute default domains, and unique the x-domain.
+	$: reactiveXDomain = reactiveXVals; // an array of (ordinal) x-values // sort by descending frequency
+	$: reactiveYDomain = [0, Math.max(...reactiveYVals)]; // [y-min, y-max]
 
-	$: innerWidth = width - (padding.left + padding.right);
-	$: barWidth = innerWidth / xTicks.length;
+	// Construct scales, axes, and formats.
+	const xRange = [marginLeft, width - marginRight]; // [left, right]
+	const yRange = [height - marginBottom, marginTop * 2]; // [bottom, top]
+	$: reactiveXScale = scaleBand(reactiveXDomain, xRange).padding(xPadding);
+	$: reactiveYScale = scaleLinear(reactiveYDomain, yRange).nice();
+
+	$: reactiveYTicks = reactiveYScale.ticks(yScaleFactor);
+	$: reactiveYTicksFormatted = reactiveYTicks.map((el) => el.toLocaleString("en-US"));
 </script>
 
-<section class="mb-24">
-	<h2>Distribution of Songs by Tempo</h2>
-
+<section class="p-5">
 	<div
-		class="chart"
-		bind:clientWidth={width}
-		bind:clientHeight={height}
+		class="chart-container"
+		dir="auto"
 	>
-		<svg>
-			<!-- y axis -->
-			<g class="axis y-axis">
-				{#each yTicks as tick}
-					<g
-						class="tick tick-{tick}"
-						transform="translate(0, {yScale(tick)})"
-					>
-						<line x2="100%" />
-						<text y="-4">{tick} songs</text>
-					</g>
-				{/each}
-			</g>
-
-			<!-- x axis -->
-			<g class="axis x-axis">
-				{#each Object.keys(tempoDistributions) as tempoBucket, i}
+		<svg
+			{width}
+			{height}
+			viewBox="0 0 {width} {height}"
+		>
+			<g
+				class="x-axis"
+				transform="translate(0,{height - marginBottom})"
+			>
+				<path
+					class="domain"
+					stroke="white"
+					d="M{marginLeft}, 0.5 H{width}"
+				/>
+				{#each reactiveXVals as xVal, i}
 					<g
 						class="tick"
-						transform="translate({xScale(i)},{height})"
+						opacity="1"
+						transform="translate({reactiveXScale(xVal)},0)"
 					>
+						<line
+							x1={reactiveXScale.bandwidth() / 2}
+							x2={reactiveXScale.bandwidth() / 2}
+							stroke="white"
+							y2="6"
+						/>
 						<text
-							x={barWidth / 2}
-							y="-4">{tempoBucket}</text
+							y={marginBottom}
+							dx={reactiveXScale.bandwidth() / 4}>{xVal}</text
 						>
 					</g>
 				{/each}
 			</g>
 
+			<g
+				class="y-axis"
+				transform="translate({marginLeft}, 0)"
+			>
+				{#each reactiveYTicks as tick, i}
+					<g
+						class="tick"
+						opacity="1"
+						transform="translate(0, {reactiveYScale(tick)})"
+					>
+						<line
+							class="tick-grid"
+							x2={width - marginLeft - marginRight}
+						/>
+						<text
+							x={-marginLeft}
+							y="10">{reactiveYTicksFormatted[i] + yFormat}</text
+						>
+					</g>
+				{/each}
+				<text
+					x="-{marginLeft}"
+					y={marginTop}
+					fill={"white"}>{yLabel}</text
+				>
+			</g>
+
 			<g class="bars">
-				{#each Object.values(tempoDistributions) as count, i}
+				{#each reactiveYVals as bar, i (bar)}
 					<rect
-						x={xScale(i) + 2}
-						y={yScale(count)}
-						width={barWidth - 4}
-						height={yScale(0) - yScale(count)}
-						in:slide={{ duration: 1000 }}
+						x={reactiveXScale(reactiveXVals[i])}
+						y={reactiveYScale(reactiveYVals[i])}
+						width={reactiveXScale.bandwidth()}
+						height={reactiveYScale(0) - reactiveYScale(bar)}
+						fill={colors[i % colors.length]}
+						animate:flip={{ duration: 1000 }}
 					/>
+				{/each}
+				{#each reactiveYVals as barLabel, i (barLabel)}
+					<text
+						class="text-xs italic"
+						x={reactiveXScale(reactiveXVals[i])}
+						y={reactiveYScale(reactiveYVals[i]) - 2}
+						width={reactiveXScale.bandwidth()}
+						height={reactiveYScale(0) - reactiveYScale(barLabel)}
+						fill={"white"}
+						animate:flip={{ duration: 1000 }}
+						>{i > 0 ? tempoMarkings[i - 1][0] + "-" : "<"}{tempoMarkings[i][0]} bpm</text
+					>
 				{/each}
 			</g>
 		</svg>
@@ -114,49 +172,43 @@
 </section>
 
 <style>
-	h2 {
+	.chart-container {
+		justify-content: center;
+		align-items: center;
 		text-align: center;
-	}
-
-	.chart {
-		width: 100%;
-		max-width: 700px;
-		margin: 0 auto;
+		margin: 1rem;
+		padding-right: 100px;
 	}
 
 	svg {
-		position: relative;
-		width: 100%;
-		height: 600px;
+		max-width: 100%;
+		max-height: 100%;
+		margin: auto;
+	}
+
+	.y-axis {
+		font-size: "10px";
+		text-anchor: "end";
+	}
+
+	.x-axis {
+		font-size: "10px";
+		text-anchor: "middle";
 	}
 
 	.tick {
-		font-family: Helvetica, Arial;
-		font-size: 0.725em;
-		font-weight: 200;
+		opacity: 1;
 	}
 
-	.tick line {
-		stroke: #e2e2e2;
-		stroke-dasharray: 2;
+	.tick-grid {
+		stroke: white;
+		stroke-opacity: 0.2;
+		font-size: "11px";
+		color: white;
 	}
 
 	.tick text {
-		fill: #ccc;
+		fill: white;
 		text-anchor: start;
-	}
-
-	.tick.tick-0 line {
-		stroke-dasharray: 0;
-	}
-
-	.x-axis .tick text {
-		text-anchor: middle;
-	}
-
-	.bars rect {
-		fill: #a11;
-		stroke: none;
-		opacity: 0.65;
 	}
 </style>
